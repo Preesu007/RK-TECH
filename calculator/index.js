5d41ad5075b9790d4d9ec192517dc05c3a813ab8
preethi sen <115079857+Preesu007@users.noreply.github.com>
2024-03-22 15:39:49 +0530

    Add files via upload

---
+++ calculator/index.js
@@ -0,0 +1,20 @@
+let result = document.getElementById("result");
+
+function addToResult(value) {
+  result.value += value;
+}
+function clearResult() {
+  result.value = "";
+}
+
+function deleteChar() {
+  result.value = result.value.slice(0, -1);
+}
+
+function calculateResult() {
+  try {
+    result.value = eval(result.value);
+  } catch (error) {
+    result.value = "Error";
+  }
+}
